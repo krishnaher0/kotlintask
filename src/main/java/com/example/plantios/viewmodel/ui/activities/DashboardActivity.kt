721dbb5987949fa42e861ff06bc600b80c572c0a
krishnaher0 <krishna613460@gmail.com>
2024-06-18 21:31:16 +0545

    Navigation Bar setup in the dashboard section

--- src/main/java/com/example/plantios/viewmodel/ui/activities/DashboardActivity.kt
+++ src/main/java/com/example/plantios/viewmodel/ui/activities/DashboardActivity.kt
@@ -42,7 +42,7 @@ class DashboardActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,0)
             insets
         }
 
